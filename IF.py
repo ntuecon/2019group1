5edb80a0208efeb3d43705ff3fb506bb751d5cb5
ova0115 <26438404+ova0115@users.noreply.github.com>
2019-05-01 13:34:33 +0800

    Add files via upload

--- IF.py
+++ IF.py
@@ -5,11 +5,11 @@ class C_B_U():#cob douglus utility function
         self.beta = beta
         self.c = c
     def optQa(self, Pa, Pb, w, pi):
-        return pow((pow((beta/Pb), alpha)*pow((Pa/alpha), (beta-1))*w/c), (1/(1-beta-alpha)))
+        return ((alpha/Pa)**alpha*(Pb/beta)**(alpha-1)*w/c)**(1/(1-beta))
     def optQb(self, Pa, Pb, w, pi):
-        return pow((pow((Pa/alpha), alpha)*pow((beta/Pb), (alpha-1))*w/c), (1/(1-beta-alpha)))
+        return ((beta/Pb)**alpha*(Pb/beta)**(alpha-1)*w/c)**(1/(1-alpha))
     def optH(self, Pa, Pb, w, pi):
-        return pow(((alpha+beta)/alpha*(pow((beta/Pb), beta)*pow((Pa/alpha), (beta-1))*w/c)), (1/(1-beta-alpha)))*Pa/w-pi/w
+        return (((alpha/Pa)**alpha*(Pb/beta)**(alpha-1)*w/c)*Pa+((beta/Pb)**alpha*(Pb/beta)**(alpha-1)*w/c)*Pb-pi)/w
 
 class SupplyFunction():
     def __init__(self, ca, cb):
@@ -47,13 +47,13 @@ Pa = ca+1
 Pb = cb+1
 w = c+1
 test = tryIt(S, C, Pa, Pb, w)
-for i in range(100):
+for i in range(10000):
     w2 = w+1
     test2 = tryIt(S, C, Pa, Pb, w2)
     if abs(test2[2][0]-test2[2][1]) < abs(test[2][0]-test[2][1]):
         w = w2
         test = test2
-for i in range(100):
+for i in range(10000):
     test = tryIt(S, C, Pa, Pb, w)
     if test[0][0]> test[0][1]:
         Pa += 1
@@ -63,3 +63,8 @@ for i in range(100):
         Pb += 1
     elif test[1][0]< test[1][1]:
         Pb -= 1
+pi = S.optPi(Pa, Pb, w)
+Qa = C.optQa(Pa, Pb, w, pi)
+Qb = C.optQb(Pa, Pb, w, pi)
+H = S.optH(Pa, Pb, w)
+print "at equilibrium there will be %f amounts of a at price %d and %f amounts of b at price %d, workers will work at the wage of %f and be payed %d per hour." % (Qa, Pa, Qb, Pb, H, w)
